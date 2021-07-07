a4b9c7eaad768aa749937e413244436b84dcfcb9
Kevin Zehnder <51463990+k-zehnder@users.noreply.github.com>
2021-07-07 08:21:12 -0700

    Update schema.py

--- schema.py
+++ schema.py
@@ -1,27 +1,39 @@
-# flask_sqlalchemy/schema.py
-import graphene
-from graphene import relay
-from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from models import db_session, Department as DepartmentModel, Employee as EmployeeModel
-
-
-class Department(SQLAlchemyObjectType):
+# Schema Objects
+class BookObject(SQLAlchemyObjectType):
     class Meta:
-        model = DepartmentModel
-        interfaces = (relay.Node, )
-
-
-class Employee(SQLAlchemyObjectType):
-    class Meta:
-        model = EmployeeModel
-        interfaces = (relay.Node, )
-
+        model = Book
+        interfaces = (graphene.relay.Node, )
+class UserObject(SQLAlchemyObjectType):
+   class Meta:
+       model = User
+       interfaces = (graphene.relay.Node, )
 
 class Query(graphene.ObjectType):
-    node = relay.Node.Field()
-    # Allows sorting over multiple columns, by default over the primary key
-    all_employees = SQLAlchemyConnectionField(Employee.connection)
-    # Disable sorting over this field
-    all_departments = SQLAlchemyConnectionField(Department.connection, sort=None)
+    node = graphene.relay.Node.Field()
+    all_books = SQLAlchemyConnectionField(BookObject)
+    all_users = SQLAlchemyConnectionField(UserObject)
 
 schema = graphene.Schema(query=Query)
+
+class AddBook(graphene.Mutation):
+    class Arguments:
+        title = graphene.String(required=True)
+        description = graphene.String(required=True) 
+        year = graphene.Int(required=True) 
+        username = graphene.String(required=True)
+    book = graphene.Field(lambda: BookObject)
+
+    def mutate(self, info, title, description, year, username):
+        user = User.query.filter_by(username=username).first()
+        book = Book(title=title, description=description, year=year)
+        if user is not None:
+            book.author = user
+        db.session.add(book)
+        db.session.commit()
+        return AddBook(book=book)
+
+class Mutation(graphene.ObjectType):
+    add_book = AddBook.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
